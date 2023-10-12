f243e7dae86b6edd80a276fb090c39ae00a0e54f
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-12 08:58:50 +0800

    add Contact component

---
+++ components/Contact.js
@@ -0,0 +1,16 @@
+'use client'
+
+const Contact = () => {
+  return (
+    <div className="contact mt-20 mx-auto rounded bg-white">
+      <div className="text-xl">
+        View my profile at https://www.linkedin.com/in/jdhrnndz/
+      </div>
+      <div className="text-xl">
+        or checkout my repos at https://github.com/jdhrnndz
+      </div>
+    </div>
+  )
+}
+
+export default Contact;
