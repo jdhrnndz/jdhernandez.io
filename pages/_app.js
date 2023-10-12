57b810713937c6920fb3cc2bd5b025607dafdd99
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-12 08:59:22 +0800

    add missing semicolon in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default Application;
