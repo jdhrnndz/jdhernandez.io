b3170f3274067dae8860052526f9a956a234b525
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-12 08:59:41 +0800

    implement simple navigation

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,34 @@ import Head from 'next/head'
 import AnimatedBackground from '@components/AnimatedBackground'
 import HomeBanner from '@components/HomeBanner'
 import Navigation from '@components/Navigation'
+import ProjectList from '@components/ProjectList'
+import { useState } from 'react';
+import routes from 'utils/routes';
+import Contact from '@components/Contact'
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(routes.About);
   return (
     <div className="container">
       <div className="header-wrap">
-        <div className="header">
-          <h1>JD Hernandez</h1>
+        <div className="header text-xl font-bold">
+          <h1>( ͡° ͜ʖ ͡°)</h1>
         </div>
       </div>
       <AnimatedBackground />
-      <HomeBanner />
-      <Navigation />
+      {
+        currentPage === routes.About &&
+        <HomeBanner />
+      }
+      {
+        currentPage === routes.Projects &&
+        <ProjectList />
+      }
+      {
+        currentPage === routes.Contact &&
+        <Contact />
+      }
+      <Navigation navTo={setCurrentPage} />
     </div>
   )
 }
