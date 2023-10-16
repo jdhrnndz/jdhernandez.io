a6d99e2c48249bd0a678637f9f4852e485ba6f7f
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-17 00:58:28 +0800

    update SiteHeader style

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -2,7 +2,7 @@
 
 const SiteHeader = ({ children }) => {
   return (
-    <div className="flex-none rounded-b-lg h-auto mb-4 p-2 outline outline-2 outline-celadon-green bg-white bg-hero-pattern select-none">
+    <div className="flex-initial rounded-b-lg p-2 outline outline-2 outline-celadon-green bg-white bg-hero-pattern select-none">
       {children}
     </div>
   )
