4a4e10547ac7bdd1bddb2ac1906a5466adcb6008
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-19 04:01:02 +0800

    refactor nav button styles

--- components/Navigation.js
+++ components/Navigation.js
@@ -21,14 +21,16 @@ const BUTTON_DATA = [
 
 const NavButtons = ({ title, href }) => {
   const router = useRouter();
+  const bgClass = router.asPath === href ? 'bg-celadon-green' : 'bg-white';
+  const textClass = router.asPath === href ? 'text-white' : 'text-asda-green';
 
   return (
     <Link href={href}>
-      <button className={`rounded z-20 hover:text-white ${router.asPath === href ? 'bg-celadon-green' : ''}`}>
-        <i className="hover-fill">{title}</i>
-        <i className="hover-fill secondary-fill">{title}</i>
-        <i className="inactive-fill">{title}</i>
-        <span>{title}</span>
+      <button className={`group relative rounded z-20 ${bgClass} uppercase inline-block no-underline text-center tracking-widest font-[Lekton] font-extrabold py-2 px-5 outline outline-2 outline-asda-green m-1.5 overflow-hidden`}>
+        <i className="absolute w-0 h-full bg-asda-green skew top-0 -left-5 -z-[1] transition-width duration-200 ease-in hover-fill group-hover:w-[150%]" />
+        <i className="absolute w-0 h-full bg-celadon-green skew top-0 -left-5 -z-[2] transition-width duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
+        <i className="absolute w-0 h-full top-0 left-0 -z-[3]" />
+        <span className={`uppercase no-underline text-center tracking-widest font-[Lekton] font-extrabold text-xl ${textClass} group-hover:text-white`}>{title}</span>
       </button>
     </Link>
   );
