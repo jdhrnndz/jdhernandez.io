181c109ae28c703b2236d7ede82e5a49dce5901f
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-19 02:00:24 +0800

    update contact style

--- pages/contact/Contact.js
+++ pages/contact/Contact.js
@@ -2,7 +2,7 @@
 
 export default function Contact() {
   return (
-    <div className="rounded bg-white text-celadon-green font-mono z-1 p-8 cursor-pointer outline outline-2 outline-celadon-green">
+    <div className="bg-zinc-50 text-stone-700 font-mono z-1 p-8 cursor-pointer outline outline-2 outline-celadon-green">
       <div className="text-xl">
         View my profile at https://www.linkedin.com/in/jdhrnndz/
       </div>
