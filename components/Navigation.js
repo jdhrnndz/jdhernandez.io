b9b49b18cabf9889a7356e3c664a6b8dba46f6b2
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-19 22:29:58 +0800

    tweak navigation button style

--- components/Navigation.js
+++ components/Navigation.js
@@ -26,9 +26,9 @@ const NavButtons = ({ title, href }) => {
 
   return (
     <Link href={href}>
-      <button className={`group relative rounded z-20 ${bgClass} uppercase inline-block no-underline text-center tracking-widest font-[Lekton] font-extrabold py-2 px-5 outline outline-2 outline-asda-green m-1.5 overflow-hidden`}>
-        <i className="absolute w-0 h-full bg-asda-green skew top-0 -left-5 -z-[1] transition-width duration-200 ease-in hover-fill group-hover:w-[150%]" />
-        <i className="absolute w-0 h-full bg-celadon-green skew top-0 -left-5 -z-[2] transition-width duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
+      <button className={`group relative z-20 ${bgClass} uppercase inline-block no-underline text-center tracking-widest font-[Lekton] font-extrabold py-2 px-5 outline outline-2 outline-asda-green m-1.5 overflow-hidden`}>
+        <i className="absolute w-0 h-full bg-asda-green skew top-0 -left-6 -z-[1] transition-width duration-200 ease-in hover-fill group-hover:w-[150%]" />
+        <i className="absolute w-0 h-full bg-celadon-green skew top-0 -left-6 -z-[2] transition-width duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
         <i className="absolute w-0 h-full top-0 left-0 -z-[3]" />
         <span className={`uppercase no-underline text-center tracking-widest font-[Lekton] font-extrabold text-xl ${textClass} group-hover:text-white`}>{title}</span>
       </button>
