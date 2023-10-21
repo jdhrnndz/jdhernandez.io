83427cbb938627ffce5be1f254df85864542c710
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-21 23:27:19 +0800

    update ProjectList layout and style

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,400;0,700;1,400;1,700&family=Lekton:wght@400;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,400;0,700;0,800;1,400;1,700&family=Lekton:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="manifest" href="/manifest.webmanifest" />
           <link rel="apple-touch-icon" href="/favicon.ico"/>
           <link type="text/plain" rel="author" href="/humans.txt" />
--- pages/projects/ProjectList.js
+++ pages/projects/ProjectList.js
@@ -4,6 +4,7 @@ import Badge from "@components/Badge";
 
 const PROJECTS = [
   {
+    name: 'haven-life',
     title: 'Haven Life',
     description: 'A SaaS application consisting of a web app for insurance providers to manage insurance policies, a web app for customers to apply for insurance products and organize their policies, and a collection of services for handling business logic, managing assets and documents, and integrating with third-party services.',
     skills: [
@@ -15,6 +16,7 @@ const PROJECTS = [
     company: 'Full-stack Developer @ HavenTech Asia',
   },
   {
+    name: 'aia-rewards-marketplace-cms',
     title: 'AIA Rewards Marketplace CMS',
     description: 'A headless CMS for the items redeemable within the AIA rewards system. The CMS is made using dotCMS whose UI is used by the marketing managers. A lightweight REST API using Express.js is created to serve the CMS content as JSON.',
     skills: [
@@ -26,6 +28,7 @@ const PROJECTS = [
     company: 'Back-end Developer @ VISEO Hong Kong Ltd.',
   },
   {
+    name: 'project-emma-axa-hk',
     title: 'Project EMMA @ AXA HK',
     description: 'A module in a mobile app that serves as a storage for information and documents pertinent to the user\'s family members and assets.',
     skills: [
@@ -37,6 +40,7 @@ const PROJECTS = [
     company: 'Front-end Developer @ VISEO Philippines Inc.',
   },
   {
+    name: 'hct-life-axa-hk',
     title: 'HCT Life @ AXA HK',
     description: 'A web app that helps to explore the network of doctors and hospitals working with the insurance provider.',
     skills: [
@@ -48,6 +52,7 @@ const PROJECTS = [
     company: 'Front-end Developer @ VISEO Philippines Inc.',
   },
   {
+    name: 'project-habit',
     title: 'Project HaBit',
     description: 'A mobile app where employees can give a colleague a token of appreciation for a job well done.',
     skills: [
@@ -56,6 +61,7 @@ const PROJECTS = [
     company: 'Android Developer @ Stratpoint Technologies Inc.',
   },
   {
+    name: 'project-sofa',
     title: 'Project SoFa',
     description: 'A mobile app where subscribers manage their broadband account.',
     skills: [
@@ -66,6 +72,7 @@ const PROJECTS = [
     company: 'Back-end Developer @ Stratpoint Technologies Inc.',
   },
   {
+    name: 'project-unimart',
     title: 'Project Unimart',
     description: 'An e-commerce website that provides subscribers easy access to subscriptions and enables resellers to track customer registrations and sales.',
     skills: [
@@ -78,6 +85,7 @@ const PROJECTS = [
     company: 'Front-end Developer @ Stratpoint Technologies Inc.',
   },
   {
+    name: 'batasqc',
     title: 'BatasQC',
     description: 'A mobile app that provides access to all ordinances and announcements within Quezon City, Philippines.',
     skills: [
@@ -89,6 +97,7 @@ const PROJECTS = [
     company: 'Freelance Android Developer',
   },
   {
+    name: 'personnel-management-system',
     title: 'Personnel Management System',
     description: 'A website for managing personnels\' information and their departments, contributions, and violations. Admins have access to audit logs, statistics dashboard, and user privileges.',
     skills: [
@@ -101,41 +110,44 @@ const PROJECTS = [
   }
 ];
 
-const Project = ({title, description, skills, company}) => {
+const Project = ({name, title, description, skills, company}) => {
   return (
     title &&
-    <article className="h-fit w-full text-stone-500 font-[Urbanist] bg-zinc-50 outline outline-2 outline-celadon-green/30 overflow-hidden">
-      <hgroup>
-        <header className="text-2xl text-white tracking-tight font-[Urbanist] font-bold bg-celadon-green w-fit h-fit pl-8 pr-4 py-1 relative after:bg-inherit after:skew after:h-full after:absolute after:w-12 after:-top-0 after:-right-2 after:shadow-stripes z-0 after:-z-[1]">{title}</header>
-      </hgroup>
-      <div className="px-8 py-4">
-        <main className="text-xl font-bold mb-8 leading-relaxed">
-          {description}
-        </main>
-        <section className="flex flex-wrap gap-2 justify-center">
-          {
-            skills.length > 0 &&
-            skills.map((skillName) => (
-              <Badge key={skillName} fontClass='font-medium'>
-                {skillName}
-              </Badge>
-            ))
-          }
-        </section>
+    <article className="group basis-full h-fit w-full text-stone-500 font-[Urbanist] bg-zinc-50 outline outline-2 outline-celadon-green/30 grid grid-rows-1 grid-cols-6 grid-flow-col" >
+      <div className="col-span-2 w-full min-h-80 max-h-96 overflow-hidden shadow-inner">
+        <img alt={`Preview of ${title}`} src={`/project-previews/${name}.png`} className="object-cover w-full h-full transition-transform group-hover:scale-125 group-hover:transition-transform bg-hero-pattern" />
       </div>
-      <footer className="bg-celadon-green/10 px-4 py-2 text-center">
-        {company}
-      </footer>
+      <div className="col-span-4 flex flex-col justify-between">
+        <div className="px-12 py-8">
+          <header className="text-2xl text-celadon-green font-[Urbanist] font-extrabold w-fit h-fit group-hover:decoration-double group-hover:underline">{title}</header>
+          <main className="text-xl font-bold mb-8 leading-relaxed">
+            {description}
+          </main>
+          <section className="flex flex-wrap gap-2 justify-center">
+            {
+              skills.length > 0 &&
+              skills.map((skillName) => (
+                <Badge key={skillName} fontClass='font-medium'>
+                  {skillName}
+                </Badge>
+              ))
+            }
+          </section>
+        </div>
+        <footer className="bg-celadon-green/10 px-4 py-2 text-center align-bottom">
+          {company}
+        </footer>
+      </div>
     </article>
   );
 };
 
 export default function ProjectList() {
   return (
-    <div className="grid 2xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 flex-wrap place-items-start gap-4 pb-12 selection:bg-asda-green/50">
+    <div className="flex flex-wrap place-items-start gap-4 pb-12 selection:bg-asda-green/50">
       {
-        PROJECTS.map((item, index) => (
-          <Project key={index} {...item} />
+        PROJECTS.map((item) => (
+          <Project key={item.name} {...item} />
         ))
       }
     </div>
