afa40f700d48c3fcbb752ea3f483fb8b21581067
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-23 23:14:19 +0800

    tweak Contact style

--- pages/contact/Contact.js
+++ pages/contact/Contact.js
@@ -12,7 +12,10 @@ export default function Contact() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => { myForm.reset(); })
+      .then(() => { 
+        alert('Your message has been sent!');
+        myForm.reset();
+      })
       .catch((error) => alert(error));
   };
 
@@ -24,11 +27,11 @@ export default function Contact() {
             Send me a message!
           </header>
         </hgroup>
-        <form name="contact" data-netlify="true" netlify="true" className="flex flex-wrap flex-row gap-4 p-8" onSubmit={handleSubmit}>
+        <form name="contact" data-netlify="true" netlify="true" className="flex flex-wrap flex-row gap-4 p-8 max-h-full" onSubmit={handleSubmit}>
           <input type="hidden" name="form-name" value="contact" />
           <input name="name" type="text" placeholder="John Doe" className="form-input basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
           <input name="email" type="email" placeholder="johndoe@mail.com" className="form-input basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
-          <textarea name="message" placeholder="What's on your mind?" className="form-textarea basis-full col-span-2 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
+          <textarea name="message" placeholder="What's on your mind?" rows="5" className="form-textarea resize-none basis-full col-span-2 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
           <button type="submit" className="w-fit h-fit mx-auto form-input border-transparent col-span-2 align-middle group relative z-20 bg-white uppercase inline-block no-underline text-center tracking-widest py-2 px-5 outline outline-2 outline-asda-green overflow-hidden">
             <span className="absolute w-0 h-full bg-asda-green skew top-0 -left-6 -z-[1] transition-width duration-200 ease-in hover-fill group-hover:w-[150%]" />
             <span className="absolute w-0 h-full bg-celadon-green skew top-0 -left-6 -z-[2] transition-width duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
