04796ead515bd8a4576b2121990a3a55fb9769c1
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-26 05:07:38 +0800

    remove unused classes in Navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -31,8 +31,8 @@ const NavButtons = ({ title, href }) => {
   return (
     <Link href={href}>
       <button className={`group relative z-20 ${bgClass} uppercase inline-block no-underline text-center tracking-widest py-2 px-5 outline outline-2 outline-asda-green m-1.5 overflow-hidden`}>
-        <span className="absolute w-0 h-full bg-asda-green skew top-0 -left-6 -z-[1] transition-width duration-200 ease-in hover-fill group-hover:w-[150%]" />
-        <span className="absolute w-0 h-full bg-celadon-green skew top-0 -left-6 -z-[2] transition-width duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
+        <span className="absolute w-0 h-full bg-asda-green skew top-0 -left-6 -z-[1] duration-200 ease-in group-hover:w-[150%]" />
+        <span className="absolute w-0 h-full bg-celadon-green skew top-0 -left-6 -z-[2] duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
         <span className="absolute w-0 h-full top-0 left-0 -z-[3]" />
         <span className={`uppercase no-underline text-center tracking-widest font-[Lekton] font-bold text-xl ${textClass} group-hover:text-white`}>{title}</span>
       </button>
