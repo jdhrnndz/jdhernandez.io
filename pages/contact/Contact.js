b29a92df821848cc9bd6412ecd7d80a6ea0e3ce2
JD Hernandez <johndeniellehernandez@gmail.com>
2023-10-26 05:06:11 +0800

    require all fields in Contact

--- pages/contact/Contact.js
+++ pages/contact/Contact.js
@@ -28,13 +28,13 @@ export default function Contact() {
           </header>
         </hgroup>
         <form name="contact" data-netlify="true" netlify="true" className="flex flex-wrap flex-row gap-4 p-8 max-h-full" onSubmit={handleSubmit}>
-          <input type="hidden" name="form-name" value="contact" />
-          <input name="name" type="text" placeholder="John Doe" className="form-input basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
-          <input name="email" type="email" placeholder="johndoe@mail.com" className="form-input basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
-          <textarea name="message" placeholder="What's on your mind?" rows="5" className="form-textarea resize-none basis-full col-span-2 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
+          <input type="hidden" required name="form-name" value="contact" />
+          <input name="name" required type="text" placeholder="John Doe" className="form-input basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
+          <input name="email" required type="email" placeholder="johndoe@mail.com" className="form-input basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
+          <textarea name="message" required placeholder="What's on your mind?" rows="5" className="form-textarea resize-none basis-full col-span-2 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10"/>
           <button type="submit" className="w-fit h-fit mx-auto form-input border-transparent col-span-2 align-middle group relative z-20 bg-white uppercase inline-block no-underline text-center tracking-widest py-2 px-5 outline outline-2 outline-asda-green overflow-hidden">
-            <span className="absolute w-0 h-full bg-asda-green skew top-0 -left-6 -z-[1] transition-width duration-200 ease-in hover-fill group-hover:w-[150%]" />
-            <span className="absolute w-0 h-full bg-celadon-green skew top-0 -left-6 -z-[2] transition-width duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
+            <span className="absolute w-0 h-full bg-asda-green skew top-0 -left-6 -z-[1] duration-200 ease-in group-hover:w-[150%]" />
+            <span className="absolute w-0 h-full bg-celadon-green skew top-0 -left-6 -z-[2] duration-300 ease-in group-hover:duration-100 group-hover:ease-in-out group-hover:w-[150%]" />
             <span className="absolute w-0 h-full top-0 left-0 -z-[3]" />
             <span className="uppercase no-underline text-center tracking-widest font-[Lekton] font-bold text-xl text-asda-green group-hover:text-white">Send</span>
           </button>
