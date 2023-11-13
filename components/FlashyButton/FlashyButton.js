a8dae95f0af6fadea0730a866d1bbe411e76e7cf
JD Hernandez <johndeniellehernandez@gmail.com>
2023-11-13 22:31:23 +0800

    revamp Contact and FlashyButton

--- components/FlashyButton/FlashyButton.js
+++ components/FlashyButton/FlashyButton.js
@@ -1,35 +1,32 @@
 'use client'
 
-import FlashyButtonStyles from "./FlashyButton.module.css";
 import classnames from "classnames";
 
 const FlashyButton = ({ type, isActive, children, ...rest }) => {
   const buttonClasses = classnames([
-    FlashyButtonStyles.base,
-    isActive ?'bg-celadon-green' : 'bg-white',
+    'relative h-fit w-fit z-20 uppercase inline-block no-underline text-center tracking-widest whitespace-nowrap py-2 px-5 outline outline-2 outline-celadon-green m-1.5 overflow-clip rounded-md transition-width',
+    isActive ? 'bg-celadon-green' : 'bg-white',
     'group',
   ]);
 
   const textClasses = classnames([
-    FlashyButtonStyles.text,
+    'uppercase no-underline text-center tracking-widest font-[Lekton] font-bold text-xl',
     isActive ? 'text-white' : 'text-celadon-green',
     'group-hover:text-white',
   ]);
 
   const firstEffectClasses = classnames([
-    FlashyButtonStyles.effect_base,
-    FlashyButtonStyles.first_effect,
-    'skew',
-    'group-hover:w-[150%]',
+    'bg-celadon-green -z-[1] duration-200',
+    'absolute w-full h-full top-0 -left-[calc(100%+3rem)] ease-in',
+    'after:absolute after:w-full after:h-full after:skew after:bg-inherit after:left-6 after:top-0',
+    'group-hover:left-0',
   ]);
 
   const secondEffectClasses = classnames([
-    FlashyButtonStyles.effect_base,
-    FlashyButtonStyles.second_effect,
-    'skew',
-    'group-hover:w-[150%]',
-    'group-hover:duration-100',
-    'group-hover:ease-in-out',
+    'bg-asda-green -z-[2] duration-300',
+    'absolute w-full h-full top-0 -left-[calc(100%+3rem)] ease-in',
+    'after:absolute after:w-full after:h-full after:skew after:bg-inherit after:left-6 after:top-0',
+    'group-hover:left-0 group-hover:duration-100 group-hover:ease-in-out',
   ]);
 
   return ( // TODO: identify the magic props here, i.e. in the rest variable
--- pages/contact/Contact.js
+++ pages/contact/Contact.js
@@ -1,10 +1,23 @@
 'use client'
 
+import { BlobEnum } from "@components/BlobThingy";
 import FlashyButton from "@components/FlashyButton";
 import { MyMagicString } from "@components/enums";
+import { useState } from "react";
+
+const ButtonState = {
+  Loading: 'loading',
+  Idle: 'idle',
+  Done: 'done',
+}
 
 export default function Contact() {
+  const [buttonState, setButtonState] = useState(ButtonState.Done);
+
   const handleSubmit = (event) => {
+    if (buttonState !== ButtonState.Idle) return;
+
+    setButtonState(ButtonState.Loading);
     event.preventDefault();
 
     const myForm = event.target;
@@ -16,21 +29,42 @@ export default function Contact() {
       body: new URLSearchParams(formData).toString(),
     })
       .then(() => {
-        alert(MyMagicString.MessageSent);
-        myForm.reset();
+        setButtonState(ButtonState.Done);
       })
-      .catch((error) => alert(error));
+      .catch((error) => alert(error))
+      .finally(() => {
+        myForm.reset();
+      });
   };
 
+  const handleSuccessAnimationEnd = () => {
+    setButtonState(ButtonState.Idle);
+  }
+
+  const getButtonChildren = (state) => {
+    switch(state) {
+      case ButtonState.Loading:
+        return <svg className="fill-celadon-green group-hover:fill-white animate-spin h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" /></svg>
+      case ButtonState.Done:
+        return <svg onAnimationEnd={handleSuccessAnimationEnd} className="fill-celadon-green group-hover:fill-white animate-fullsend h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg>
+      case ButtonState.Idle:
+      default:
+        return MyMagicString.Contact.cta;
+    }
+  }
+
   return (
-    <div className="flex flex-col mx-auto h-full justify-center w-fit sm:w-8/12 gap-4">
-      <div className="bg-zinc-50 text-stone-700 font-[Urbanist] outline outline-2 outline-celadon-green/30 selection:bg-asda-green/50 overflow-hidden">
-        <hgroup className="relative bg-celadon-green w-fit h-fit px-8 py-4 after:bg-celadon-green after:skew after:h-full after:absolute after:w-12 after:-top-0 after:-right-2 after:shadow-stripes z-0 after:-z-[1]">
-          <header className="text-lg sm:text-2xl text-white font-bold tracking-wide">
-            {MyMagicString.Contact.header}
-          </header>
-        </hgroup>
-        <form name="contact" data-netlify="true" netlify="true" className="flex flex-wrap flex-row gap-4 p-8 max-h-full" onSubmit={handleSubmit}>
+    <div className="flex flex-col mx-auto h-full justify-center w-fit sm:w-8/12 px-12 py-8">
+      <header className="bg-celadon-green w-fit h-fit px-4 py-2 sm:px-8 sm:py-4 rounded-full text-lg sm:text-2xl text-white font-bold tracking-wide">
+        {MyMagicString.Contact.header}
+      </header>
+      <div class="w-fit h-fit grid grid-rows-[1fr_auto] grid-cols-6">
+        <BlobEnum.LeftConnector width="w-16" height="h-4" />
+        <BlobEnum.ConnectorFill width="w-16" height="h-4" />
+        <BlobEnum.RightConnector width="w-16" height="h-4" />
+      </div>
+      <div className="text-stone-700 font-[Urbanist] selection:bg-asda-green/50">
+        <form name="contact" data-netlify="true" netlify="true" className="flex flex-wrap flex-row gap-4 max-h-full" onSubmit={handleSubmit}>
           <input
             type="hidden"
             required
@@ -42,26 +76,27 @@ export default function Contact() {
             required
             type="text"
             placeholder={MyMagicString.Contact.placeholder.name}
-            className="form-input focus:border-asda-green basis-full sm:basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10 text-base sm:text-xl font-bold tracking-wide"
+            className="form-input px-4 py-2 sm:px-8 sm:py-4 focus:border-asda-green basis-full sm:basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green shadow-inner bg-zinc-100 text-base sm:text-xl font-bold tracking-wide rounded-full"
           />
           <input
             name="email"
             required
             type="email"
             placeholder={MyMagicString.Contact.placeholder.email}
-            className="form-input focus:border-asda-green basis-full sm:basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10 text-base sm:text-xl font-bold tracking-wide"
+            className="form-input px-4 py-2 sm:px-8 sm:py-4 focus:border-asda-green basis-full sm:basis-1/2-gap-4 border-transparent outline outline-2 outline-celadon-green shadow-inner bg-zinc-100 text-base sm:text-xl font-bold tracking-wide rounded-full"
           />
           <textarea
             name="message"
             required
             placeholder={MyMagicString.Contact.placeholder.message}
             rows="5"
-            className="form-textarea focus:border-asda-green resize-none basis-full border-transparent outline outline-2 outline-celadon-green/30 shadow-inner bg-celadon-green/10 text-base sm:text-xl font-bold tracking-wide"
+            className="form-textarea px-4 py-2 sm:px-8 sm:py-4 focus:border-asda-green resize-none basis-full border-transparent outline outline-2 outline-celadon-green shadow-inner bg-zinc-100 text-base sm:text-xl font-bold tracking-wide rounded-3xl"
           />
-          <div className="mx-auto">
-            <FlashyButton type="submit">
-              {MyMagicString.Contact.cta}
+          <div className="mx-auto h-16">
+            <FlashyButton type={buttonState === ButtonState.Idle ? 'submit' : ''}>
+              {getButtonChildren(buttonState)}
             </FlashyButton>
+            <div className="h-[150%]"/>
           </div>
         </form>
       </div>
