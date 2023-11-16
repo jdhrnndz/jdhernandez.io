d876e2e60a11509695d7e1fcf761f2823b1e4d64
JD Hernandez <johndeniellehernandez@gmail.com>
2023-11-16 13:07:30 +0800

    get rid of CSS module of Badge

--- components/Badge.js
+++ components/Badge.js
@@ -1,19 +1,27 @@
-import BadgeStyles from './Badge.module.css';
 import classnames from 'classnames';
 
 const Badge = ({ children, fontClass, active }) => {
   const classes = classnames([
-    BadgeStyles.base,
     fontClass,
-    'after:content-[attr(after)]',
-    'before:skew',
+    'font-[Lekton] select-none inline-flex items-center rounded-full px-4 py-1 ring-2 ring-inset ring-celadon-green/50 bg-celadon-green/10 text-celadon-green relative z-0 overflow-hidden',
     {
-      [BadgeStyles.active]: active,
+      'text-white': active,
+    }
+  ]);
+
+  const sweepEffectClasses = classnames([
+    'bg-celadon-green -z-10 duration-200',
+    'absolute w-full h-full top-0 ease-in',
+    'after:absolute after:w-full after:h-full after:skew after:bg-inherit after:left-6 after:top-0',
+    {
+      'left-0': active,
+      '-left-[calc(100%+3rem)]': !active,
     },
   ]);
 
   return (
     <span className={classes} after={children}>
+      <span className={sweepEffectClasses} />
       {children}
     </span>
   )
